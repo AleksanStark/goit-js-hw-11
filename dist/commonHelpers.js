00840ea3eb104ec56433944c10481f652d676f00
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-02-23 19:02:05 +0900

    complete build with vite

--- dist/commonHelpers.js
+++ dist/commonHelpers.js
@@ -1,87 +1,90 @@
-import { S as a, i as c } from '../assets/vendor-5b791d57.js';
+import { S as u, i as m } from '../assets/vendor-5b791d57.js';
 (function () {
-  const s = document.createElement('link').relList;
-  if (s && s.supports && s.supports('modulepreload')) return;
-  for (const e of document.querySelectorAll('link[rel="modulepreload"]')) n(e);
+  const t = document.createElement('link').relList;
+  if (t && t.supports && t.supports('modulepreload')) return;
+  for (const e of document.querySelectorAll('link[rel="modulepreload"]')) r(e);
   new MutationObserver(e => {
-    for (const t of e)
-      if (t.type === 'childList')
-        for (const l of t.addedNodes)
-          l.tagName === 'LINK' && l.rel === 'modulepreload' && n(l);
+    for (const s of e)
+      if (s.type === 'childList')
+        for (const n of s.addedNodes)
+          n.tagName === 'LINK' && n.rel === 'modulepreload' && r(n);
   }).observe(document, { childList: !0, subtree: !0 });
-  function r(e) {
-    const t = {};
+  function l(e) {
+    const s = {};
     return (
-      e.integrity && (t.integrity = e.integrity),
-      e.referrerPolicy && (t.referrerPolicy = e.referrerPolicy),
+      e.integrity && (s.integrity = e.integrity),
+      e.referrerPolicy && (s.referrerPolicy = e.referrerPolicy),
       e.crossOrigin === 'use-credentials'
-        ? (t.credentials = 'include')
+        ? (s.credentials = 'include')
         : e.crossOrigin === 'anonymous'
-        ? (t.credentials = 'omit')
-        : (t.credentials = 'same-origin'),
-      t
+        ? (s.credentials = 'omit')
+        : (s.credentials = 'same-origin'),
+      s
     );
   }
-  function n(e) {
+  function r(e) {
     if (e.ep) return;
     e.ep = !0;
-    const t = r(e);
-    fetch(e.href, t);
+    const s = l(e);
+    fetch(e.href, s);
   }
 })();
-const u = (i, s) => {
-    const r = new a('.list-link');
-    if (((s.innerHTML = ''), i.length === 0))
-      return c.error({
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-        position: 'topRight',
-      });
-    const n = i
+const p = i =>
+    fetch(
+      `https://pixabay.com/api/?key=42464755-f7d199d1a91f6070a7f813e04&image_type=photo&orientation=horizontal&q=${i}&safesearch=true`
+    ),
+  d = new u('.list-link'),
+  f = (i, t) => {
+    const l = i
       .map(
-        e => `<li class="list-item">
-            <a class="list-link" href="${e.largeImageURL}"><img class="list-img" src="${e.webformatURL}" data-original="${e.largeImageURL}" download></a>
+        r => `<li class="list-item">
+            <a class="list-link" href="${r.largeImageURL}"><img class="list-img" src="${r.webformatURL}" data-original="${r.largeImageURL}" download></a>
             <ul class="list-statistic">
               <li>
                 <span class="list-item-title">likes</span>
-                <span class="list-item-number">${e.likes}</span>
+                <span class="list-item-number">${r.likes}</span>
               </li>
   
               <li>
                 <span class="list-item-title">views</span>
-                <span class="list-item-number">${e.views}</span>
+                <span class="list-item-number">${r.views}</span>
               </li>
               <li>
                 <span class="list-item-title">comments</span>
-                <span class="list-item-number">${e.comments}</span>
+                <span class="list-item-number">${r.comments}</span>
               </li>
               <li>
                 <span class="list-item-title">downloads</span>
-                <span class="list-item-number">${e.downloads}</span>
+                <span class="list-item-number">${r.downloads}</span>
               </li>
             </ul>
           </li>`
       )
       .join('');
-    s.insertAdjacentHTML('beforeend', n), r.refresh();
+    t.insertAdjacentHTML('beforeend', l), d.refresh();
   },
   o = document.querySelector('.form'),
-  m = document.querySelector('.list');
+  a = document.querySelector('.list'),
+  c = document.querySelector('.loader-container');
 o.addEventListener('submit', i => {
-  i.preventDefault();
-  const s = document.querySelector('.loader-container');
-  return (
-    (s.style.display = 'flex'),
-    fetch(
-      `https://pixabay.com/api/?key=42464755-f7d199d1a91f6070a7f813e04&image_type=photo&orientation=horizontal&q=${i.target.elements.search.value}&safesearch=true`
-    )
-      .then(r => {
-        if (!r.ok) throw new Error(r.status);
-        return r.json();
+  i.preventDefault(),
+    (c.style.display = 'flex'),
+    p(i.target.elements.search.value)
+      .then(t => {
+        if (t.ok) return t.json();
+        throw new Error(t.status);
       })
-      .then(r => {
-        (o.search.value = ''), u(r.hits, m), (s.style.display = 'none');
-      })
-  );
+      .then(t => {
+        t.hits.length === 0 &&
+          m.error({
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+            position: 'topRight',
+          }),
+          (o.search.value = ''),
+          (a.innerHTML = ''),
+          f(t.hits, a),
+          (c.style.display = 'none');
+      });
 });
 //# sourceMappingURL=commonHelpers.js.map
