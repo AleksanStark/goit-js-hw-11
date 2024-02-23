e746bd5b7fcfd10a56f71e9ea0525d9f6f220a12
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-02-23 18:58:55 +0900

    update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,23 +1,10 @@
 // Описаний у документації
-import iziToast from 'izitoast';
-// Додатковий імпорт стилів
-import 'izitoast/dist/css/iziToast.min.css';
-// Описаний у документації
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
+const gallery = new SimpleLightbox('.list-link');
 // renderMarkup accept list of elements and tag which insert elements fron list
 export const renderMarkup = (list, tag) => {
-  const gallery = new SimpleLightbox('.list-link');
-  tag.innerHTML = '';
-  if (list.length === 0) {
-    return iziToast.error({
-      message:
-        'Sorry, there are no images matching your search query. Please try again!',
-      position: 'topRight',
-    });
-  }
   const markup = list
     .map(image => {
       return `<li class="list-item">
