aa087a41b028029092850df9cac9ebfc6de32514
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-02-23 19:40:24 +0900

    update js files

--- src/js/pixaday-api.js
+++ src/js/pixaday-api.js
@@ -1,5 +1,11 @@
 export const fetchPosts = searchValue => {
   return fetch(
     `https://pixabay.com/api/?key=42464755-f7d199d1a91f6070a7f813e04&image_type=photo&orientation=horizontal&q=${searchValue}&safesearch=true`
-  );
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    } else {
+      return response.json();
+    }
+  });
 };
