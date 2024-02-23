d6cf6e5d03a311e15c08af7d4b4ef7009c7947b9
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-02-23 18:57:31 +0900

    update js files

--- src/main.js
+++ src/main.js
@@ -1,1 +1,36 @@
 import './js/pixaday-api.js';
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+import { renderMarkup } from './js/render-functions.js';
+import { fetchPosts } from './js/pixaday-api.js';
+const form = document.querySelector('.form');
+const listImages = document.querySelector('.list');
+const loader = document.querySelector('.loader-container');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  loader.style.display = 'flex';
+  fetchPosts(event.target.elements.search.value)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      } else {
+        return response.json();
+      }
+    })
+    .then(images => {
+      if (images.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+      }
+      form.search.value = '';
+      listImages.innerHTML = '';
+      renderMarkup(images.hits, listImages);
+      loader.style.display = 'none';
+    });
+});
