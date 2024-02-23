ce34d4034e8ffed50208ee1a4c1bbdba7e7aaf10
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-02-23 19:00:33 +0900

    update pixaday-api.js

--- src/js/pixaday-api.js
+++ src/js/pixaday-api.js
@@ -1,22 +1,5 @@
-import { renderMarkup } from './render-functions';
-const form = document.querySelector('.form');
-const listImages = document.querySelector('.list');
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const loader = document.querySelector('.loader-container');
-  loader.style.display = 'flex';
+export const fetchPosts = searchValue => {
   return fetch(
-    `https://pixabay.com/api/?key=42464755-f7d199d1a91f6070a7f813e04&image_type=photo&orientation=horizontal&q=${event.target.elements.search.value}&safesearch=true`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(images => {
-      form.search.value = '';
-      renderMarkup(images.hits, listImages);
-      loader.style.display = 'none';
-    });
-});
+    `https://pixabay.com/api/?key=42464755-f7d199d1a91f6070a7f813e04&image_type=photo&orientation=horizontal&q=${searchValue}&safesearch=true`
+  );
+};
